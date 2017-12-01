57439fbfb3f900ea545e43821e00a9d67d4a331e
Jelle den Haan <jelle_den_haan@hotmail.nl>
2017-12-01 12:05:39 +0100

    added comments to file

--- Homework/week5/d3line.js
+++ Homework/week5/d3line.js
@@ -2,28 +2,17 @@
 var parsetime = d3.timeParse("%m/%d/%Y")
 
 window.onload = function(d) {
-
-// 	var Leeuwarden = "outputl.json";
-// 	var Schiphol = "outputs.json";
-
-// 	d3.queue()
-// 		.defer(d3.json, Leeuwarden)
-// 		.defer(d3.json, Schiphol)
-// 		.await(load_data);	
-// }; 
-
-// function load_data(error, dataL, dataS) {
-//    		if (error) throw error;
     
     d3.json('outputl.json', function(error, data) {
 			if (error) throw error;
 
+    // variables to store data
     var data = data;
-
     var min_temp = [];
 	var max_temp = [];
 	var average_temp = [];
 
+	// push date to right list
 	data.forEach( function(d){
 			var date = parsetime(d.date);
 			min_temp.push({date: date, temperature: d.min_temp/10})
@@ -31,8 +20,10 @@ window.onload = function(d) {
 			average_temp.push({date: date, temperature: d.average_temp/10})
 			});
 
+	// save data in right form
 	var good_data = [{id: "min_temp", values: min_temp}, {id: "average_temp", values: average_temp}, {id: "max_temp", values: max_temp}];
 
+    // parameters for the svg and margins
     var svg = d3.select("svg"),
 	margin = {top: 30, bottom: 50, left: 50, right: 50},
 	width = svg.attr("width") - margin.left - margin.right,
@@ -40,24 +31,29 @@ window.onload = function(d) {
 	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	
+	// variables to set the x and y axis, and colors for the three graphs
 	var x = d3.scaleTime().range([0, width]),
 		y = d3.scaleLinear().range([height, 0]),
 		z = d3.scaleOrdinal(d3.schemeCategory10);
 
-  	console.log(good_data);
+  	
+  	
+  	// declare x domain 
   	x.domain(d3.extent(min_temp, function(d) { return d.date; }));
 	
-
+	// declare y domain
 	y.domain([
     d3.min(min_temp, function(d) { return d.temperature; }),
     d3.max(max_temp, function(d) { return d.temperature; })
   ]);
 
+    // append x axis
     g.append("g")
     	.attr("class", "axis axis--x")
       	.attr("transform", "translate(0," + height + ")")
       	.call(d3.axisBottom(x));
 
+  	// append y axis 
   	g.append("g")
     	.attr("class", "axis axis--y")
       	.call(d3.axisLeft(y))
@@ -70,10 +66,11 @@ window.onload = function(d) {
 
     z.domain(good_data.map(function(d) { return d.id; }));
 
+  	// variables to draw lines 
   	var line = d3.line()
 	    .curve(d3.curveBasis)
 	    .x(function(d) { return x(d.date); })
-	    .y(function(d) { console.log(d.temperature); return y(d.temperature); });
+	    .y(function(d) { return y(d.temperature); });
 
 
   	var point= g.selectAll(".point")
@@ -82,6 +79,7 @@ window.onload = function(d) {
     		.attr("class", "point" );
 
 	
+    // draw lines 
     point.append("path")
     	.attr("class", "line")
     	.attr("d", function(d) { return line(d.values); })
@@ -110,23 +108,26 @@ window.onload = function(d) {
 	// 	.style("text-anchor", "end")
 	// 	.text(function(d) { return d.id; });
 
-	console.log(d3.select("#option input"))
+	
 	d3.select("#option input")
 		.on("click", updateData);
 
 
- //    // vanaf dit punt 2 stations proberen te krijgen
+
 	function updateData() {
 
     
     // Get the data again
     d3.json("outputs.json", function(error, data) {
+    	
+    	// variables to store the data
     	var data = data;
-
 	    var min_temp = [];
 		var max_temp = [];
 		var average_temp = [];
 		var parsetime = d3.timeParse("%m/%d/%Y")
+	
+	// push date to right list
 	data.forEach( function(d){
 			var date = parsetime(d.date);
 			min_temp.push({date: date, temperature: d.min_temp/10})
@@ -134,11 +135,13 @@ window.onload = function(d) {
 			average_temp.push({date: date, temperature: d.average_temp/10})
 			});
 
+	// makes sure the data is saved properly
 	var good_data = [{id: "min_temp", values: min_temp}, {id: "average_temp", values: average_temp}, {id: "max_temp", values: max_temp}];
 	
+	// update x domain
 	x.domain(d3.extent(min_temp, function(d) { return d.date; }));
 	
-
+	// update y domain 
 	y.domain([
     d3.min(min_temp, function(d) { return d.temperature; }),
     d3.max(max_temp, function(d) { return d.temperature; })
@@ -152,16 +155,20 @@ window.onload = function(d) {
 
     d3.selectAll(".line")
     	.data(good_data);
-    // Make the changes
-        svg.selectAll(".line")   // change the line
-            .duration(750)
-            .attr("d", function(d) { return line(d.values); });
-        svg.select("axis axis--x") // change the x axis
-            .duration(750)
-            .call(x);
-        svg.select("axis axis--y") // change the y axis
-            .duration(750)
-            .call(y);
+    
+    // update the lines of the graph 
+    svg.selectAll(".line")   
+        .duration(750)
+        .attr("d", function(d) { return line(d.values); });
+    
+    // change the x axis
+    svg.select("axis axis--x") 
+        .duration(750)
+        .call(x);
+    // change the y axis 
+    svg.select("axis axis--y") 
+        .duration(750)
+        .call(y);
 
     });
 	};
